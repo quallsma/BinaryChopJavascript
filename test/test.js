59eb4a8f63857c5ba0f664cb8e198e9a5f4223e1
quallsma <quallsma@msu.edu>
2017-07-21 13:45:47 -0400

    First Commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { binaryChop } from '../src/main';
+
+describe("Mocha and Chai", () => {
+    it("should be working", () => {
+        expect(true).to.be.true;
+    });
+});
