c48d1311260f5cfb4ba316dd48d9ea56b076b394
quallsma <quallsma@msu.edu>
2017-07-24 09:15:33 -0400

    Work In Progress

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
-export const binaryChop = () => {
-    
+export const binaryChop = (num, numArray) => {
+    target = num;
+    numberArray = numArray;
+
+    let firstIndex = 0;
+    let lastIndex = numberArray.length - 1;
+    let correctIndex = -1;
+
+    if(isValid()){
+        let run = true;
+        while(run){
+            let middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+            let indexNumber = numberArray[middleIndex];
+            
+            if(indexNumber == target){
+                correctIndex = middleIndex;
+                run = false;
+            }
+            else if (indexNumber < target)
+                firstIndex = middleIndex +1;
+            else if(indexNumber > target)
+                lastIndex = middleIndex -1;
+        }
+    }
+
+    return correctIndex;
+};
+
+let target;
+let numberArray;
+
+let isValid = () => {
+    let valid = true;
+
+    if(numberArray.length == 0)
+        valid = false;
+    if(!(numberArray.includes(target)))
+        valid = false;
+
+    return valid;
 };
--- test/test.js
+++ test/test.js
@@ -6,3 +6,27 @@ describe("Mocha and Chai", () => {
         expect(true).to.be.true;
     });
 });
+
+describe("Binary Chop function", () => {
+    describe("Error Handling", () => {
+        it("should return -1 for empty array", () => {
+            expect(binaryChop(0, [])).to.equals(-1);
+        });
+
+        it("should return -1 for number not in array", () => {
+            expect(binaryChop(3, [1])).to.equals(-1);
+        });
+    });
+
+    describe("Return index of given number in array", () => {
+        it("should return 0 for single item in array matching number", () => {
+            expect(binaryChop(1, [1])).to.equals(0);
+        });
+        it("should return 1 for middle element in array", () => {
+            expect(binaryChop(2, [1, 2, 3])).to.equals(1);
+        });
+        it("should return 2 for last element in array", () => {
+            expect(binaryChop(3, [1, 2, 3])).to.equals(2);
+        });
+    });
+});
